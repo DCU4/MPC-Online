42d622f9eb09ca31186f9fbfb283a4366b976974
Dylan Connor <DylanJConnor4@gmail.com>
2018-03-30 13:56:30 +0100

    simplify js

--- main.js
+++ main.js
@@ -1,120 +1,47 @@
 var baseUrl = "./Neptunes Kit 1/";
-var audio = ["N1S11.wav", "N1S11reversed.wav","N3S25.wav",
-"N4K8.wav","N4P4.wav","N4P8.wav","N9K28.wav","N9P23.wav"];
+var audio = ["N1S11.wav", "N1S11reversed.wav","N3S25.wav", "N4K8.wav","N4P4.wav","N4P8.wav","N9K28.wav","N9P23.wav"];
+var pads = [49,50,51,52, 81,87,69,82, 65,83,68,70, 90,88,67,86];
 
+// function pads() {
+// 	var sound = Audio(baseUrl + audio);
+// 	sound.play();
+// 	sound.volume() = .5;
+// }
 
-	$(document).ready(function(){
-
-    	
-		$('.pad').each(function(){
-    	$(document).on('keydown', function(e) {  
-    	
-    for (i=0; i < 9; i++) {
-
-		switch (e.keyCode) {
-    		case 49: // 1 Key
-    		var pad = new Audio(baseUrl + audio[0]);
-    		pad.play();
-    		pad.volume = 0.01;
-    }
-
-   }
-    	
-});
-
-});
+// for(i=0; i<audio.length; i++){
+// 	pads();
+// }
 
 
 
-	$(document).on('keydown keyup', function(e){
-			
+$(document).ready(function(){
+
+$(document).on('keydown', function(e){
+
+	padDown(e);
+
+})
+})
+
+
+function padDown(e){
+	pads.forEach(function(p,i){
 		//Pads 1-4
-		if (e.keyCode === 49){
-
-			$("#pad1").toggleClass("pad-down");
-			
+		if (e.keyCode === pads[p,i]){
+			var pad = $("#pad"+i);
+			console.log(p,i);
+			$(pad).addClass("pad-down");
+		};
 		
-		};	
-		if (e.keyCode == 50){
-
-			$("#pad2").toggleClass("pad-down");
-			
-		};	
-		if (e.keyCode == 51){
-
-			$("#pad3").toggleClass("pad-down");
-		};	
-		if (e.keyCode == 52){
-
-			$("#pad4").toggleClass("pad-down");
-		};	
-
-		//Pads 5-8
-
-		if (e.keyCode == 81){
-
-			$("#pad5").toggleClass("pad-down");
-		};	
-		if (e.keyCode == 87){
-
-			$("#pad6").toggleClass("pad-down");
-		};	
-		if (e.keyCode == 69){
-
-			$("#pad7").toggleClass("pad-down");
-		};	
-		if (e.keyCode == 82){
-
-			$("#pad8").toggleClass("pad-down");
-		};	
-
-		//Pads 9-12
-
-		if (e.keyCode == 65){
-
-			$("#pad9").toggleClass("pad-down");
-		};	
-			
-		if (e.keyCode == 83){
-
-			$("#pad10").toggleClass("pad-down");
-		}
-		
-		if (e.keyCode == 68){
-
-			$("#pad11").toggleClass("pad-down");
-		};	
-
-		if (e.keyCode == 70){
-
-			$("#pad12").toggleClass("pad-down");
-		};	
-
-		//Pads 13-16
-
-		if (e.keyCode == 90){
-
-			$("#pad13").toggleClass("pad-down");
-		};	
-			
-		if (e.keyCode == 88){
-
-			$("#pad14").toggleClass("pad-down");
-		}
-		
-		if (e.keyCode == 67){
-
-			$("#pad15").toggleClass("pad-down");
-		};	
-
-		if (e.keyCode == 86){
-
-			$("#pad16").toggleClass("pad-down");
-		};	
+	}) //end forEach 
+}
 
 
-		});
-	
 
-});
-
+function listAudio() {
+	//loop through audio and add baseurl to it
+	audio.forEach(function(i,a){
+		console.log(new Audio(baseUrl + audio[a]));
+		return new Audio(baseUrl + audio[a]);
+	})
+}
