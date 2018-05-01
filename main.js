a4eab359f798e129c7fb39b3d1a104c9c8b9a4a5
Dylan Connor <DylanJConnor4@gmail.com>
2018-05-01 14:38:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,54 +1,144 @@
 var baseUrl = "./Neptunes Kit 1/";
 var audio = ["N1S11.wav", "N1S11reversed.wav","N3S25.wav", "N4K8.wav","N4P4.wav","N4P8.wav","N9K28.wav","N9P23.wav"];
-var pads = [49,50,51,52, 81,87,69,82, 65,83,68,70, 90,88,67,86];
-
-// function pads() {
-// 	var sound = Audio(baseUrl + audio);
-// 	sound.play();
-// 	sound.volume() = .5;
-// }
-
-// for(i=0; i<audio.length; i++){
-// 	pads();
-// }
+var padNum = [49,50,51,52, 81,87,69,82, 65,83,68,70, 90,88,67,86];
 
 
 
 $(document).ready(function(){
-
-$(document).on('keydown', function(e){
-
+	$(document).on('keydown keyup', function(e){
+		
 	padDown(e);
 
+	});
+
+	$(document).on('keydown', function(event){
+		
+	playAudio(event);
+
+	});  
+
+	
 })
-})
+
 
 
 function padDown(e){
-	pads.forEach(function(p,i){
-		//Pads 1-4
-		if (e.keyCode === pads[p,i]){
-			var pad = $("#pad"+i);
+	padNum.forEach(function(p,i){		
+		if (e.keyCode === padNum[p,i]){
+			var padDown = $(".pad"+i);
 			console.log(p,i);
-			$(pad).addClass("pad-down");
+			$(padDown).toggleClass("pad-down");		
 		};
-		
-	}) //end forEach 
+	}) //end pads.forEach 
+}
+
+function playAudio(event){
+	if(keyData[event.key]){
+		keyData[event.key].sound.play();
+	}
 }
 
 
+var keyData = {
+	1: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N1S11.wav']
+		}),
+		color: '#1abc9c'
+	},
+	2: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N1S11reversed.wav']
+		}),
+		color: '#1abc9c'
+	},
+	3: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N3S25.wav']
+		}),
+		color: '#1abc9c'
+	},
+	4: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N4K8.wav']
+		}),
+		color: '#1abc9c'
+	},
+	q: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N4P4.wav']
+		}),
+		color: '#1abc9c'
+	},
+	w: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N4P8.wav']
+		}),
+		color: '#2ecc71'
+	},
+	e: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N9K28.wav']
+		}),
+		color: '#3498db'
+	},
+	r: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N9P23.wav']
+		}),
+		color: '#9b59b6'
+	},
+	
+	a: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N9P23.wav']
+		}),
+		color: '#f1c40f'
+	},
+	s: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N9P23.wav']
+		}),
+		color: '#e67e22'
+	},
+	d: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N9P23.wav']
+		}),
+		color: '#e74c3c'
+	},
+	f: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N9P23.wav']
+		}),
+		color: '#95a5a6'
+	},
+	
 
-function listAudio() {
-	//loop through audio and add baseurl to it
-	audio.forEach(function(a,i){	
-			var audList = [];
-			audList[i] = new Audio();
-			audList[i].src = baseUrl + audio[a,i];
-			// console.log(audList[i]);
-			audList.forEach(function(x,y){
-				console.log(x,y);
-			})
-
-	}) // end audio.forEach
+	z: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N9P23.wav']
+		}),
+		color: '#9b59b6'
+	},
+	x: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N9P23.wav']
+		}),
+		color: '#34495e'
+	},
+	c: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N9P23.wav']
+		}),
+		color: '#16a085'
+	},
+	v: {
+		sound: new Howl({
+  		src: ['Neptunes Kit 1/N9P23.wav']
+		}),
+		color: '#27ae60'
+	}
 
 }
+
